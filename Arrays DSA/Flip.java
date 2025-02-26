30a519157e9e53cb4a930cbc785e6a04d3f24aa1
yasvanthasai <72198764+yasvanthasai@users.noreply.github.com>
2025-02-26 08:59:06 -0500

    Update Flip.java
    Added comments and description

--- Arrays DSA/Flip.java
+++ Arrays DSA/Flip.java
@@ -11,16 +11,15 @@ If you don't want to perform the operation, return an empty array. Else, return 
 NOTE: Pair (a, b) is lexicographically smaller than pair (c, d) if a < c or, if a == c and b < d.*/
 public class Solution {
     public int[] flip(String A) {
-         int n=A.length();
-        int l=0,r=0,cu=0,max=0;
-        int[] res=new int[2];
-        for(int i=0;i<n;i++){
+         int n=A.length(); //finding length
+        int l=0,r=0,cu=0,max=0; //initialization
+        int[] res=new int[2]; //answer array for the indices
+        for(int i=0;i<n;i++){ //going through all elements in array
             char ch=A.charAt(i);
-            if(ch=='1') cu-=1;
-            else  cu+=1;
-        
-            if(cu>max){
-                max=cu;
+            if(ch=='1') cu-=1; //-1 if it's a one, becasue in the end we're going to flip it 
+            else  cu+=1; //+1 becasue we can converting 0's into 1's and that will add up to the answer
+            if(cu>max){ //Kadane's algorithm here, only things is we need to trap the indices too.
+                max=cu; //it's exactly like finding the maximum subarray sum but with 0's and 1's (flip case too)
                 res[0]=l;
                 res[1]=r;
             }
@@ -31,10 +30,7 @@ public class Solution {
             else{
                 r=r+1;
             }
-        
-            
         }
-        
         res[0]+=1;
         res[1]+=1;
         if(max==0) return new int[0];
