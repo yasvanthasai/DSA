7ca75496ffb11ad4ee14ce407bcce856c10dac73
yasvanthasai <72198764+yasvanthasai@users.noreply.github.com>
2025-04-11 11:11:20 -0400

    Delete one number code

--- Maths GCD/DeleteOne.java
+++ Maths GCD/DeleteOne.java
@@ -1,3 +1,6 @@
+/*Given an integer array A of size N. You have to delete one element such that the GCD(Greatest common divisor) of the remaining array is maximum.
+
+Find the maximum value of GCD. */
 public class DeleteOne {
     public int gcd(int a,int b){
         if(a==0){
